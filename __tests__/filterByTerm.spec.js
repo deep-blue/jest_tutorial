be049ee87be8e42874d5f845271b2c8aceaba729
Frank Adler <deepblue66@gmx.net>
2020-05-12 22:36:55 +0200

    more tests

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -11,4 +11,30 @@ describe("Filter function", () => {
         const output = [{ id: 3, url: "https://www.link3.dev" }];
         expect(filterByTerm(input,"link")).toEqual(output);
     });
+    test("result is empty array if nothing matches", () => {
+        const input = [
+            { id: 1, url: "https://www.url1.dev" },
+            { id: 2, url: "https://www.url2.dev" },
+            { id: 3, url: "https://www.link3.dev" }
+          ];
+        const output = [];
+        expect(filterByTerm(input,"notexistent")).toEqual(output);
+    });
+    test("result is empty if input array is empty", () => {
+        const input = [];
+        const output = [];
+        expect(filterByTerm(input,"notexistent")).toEqual(output);
+    });
+    test("it should filter by a search term (url)", () => {
+        const input = [
+            { id: 1, url: "https://www.url1.dev" },
+            { id: 2, url: "https://www.url2.dev" },
+            { id: 3, url: "https://www.link3.dev" }
+          ];
+        const output = [
+            { id: 1, url: "https://www.url1.dev" },
+            { id: 2, url: "https://www.url2.dev" },
+        ];
+        expect(filterByTerm(input,"url")).toEqual(output);
+    });
 });
--- src/filterByTerm.js
+++ src/filterByTerm.js
@@ -1,4 +1,8 @@
 function filterByTerm(inputArr, searchTerm) {
+    if (inputArr.length == 0) {
+        // do some nonsense here
+        const bla = 'fasel';
+    };
     return inputArr.filter(function(arrayElement) {
       return arrayElement.url.match(searchTerm);
     });
